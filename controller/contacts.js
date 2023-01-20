f2901574987cf534f6fcc55d66b6b9c8c4bf87ae
Michell Herrera <her20045@byui.edu>
2023-01-20 16:42:35 +0100

    error put

--- controller/contacts.js
+++ controller/contacts.js
@@ -84,10 +84,6 @@ const getSingle = async (req, res, next) => {
     }
   };
 
-
-
-test
-.contacts
   // const deleteContact = async (req, res) => {
   //   const userId = new ObjectId(req.params.id);
   //   const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
