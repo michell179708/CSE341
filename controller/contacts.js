3c0c7cf31458ade67bfdb3fba79f45a19865c38b
Michell Herrera <her20045@byui.edu>
2023-01-21 16:24:27 +0100

    funcion delete , probada y funciona

--- controller/contacts.js
+++ controller/contacts.js
@@ -1,10 +1,11 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const { ObjectID } = require("bson");
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db().collection("contacts").find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
 };
@@ -14,86 +15,97 @@ const getSingle = async (req, res, next) => {
   const result = await mongodb
     .getDb()
     .db()
-    .collection('contacts')
+    .collection("contacts")
     .find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
 };
 
+//Explicacion de lo que hace las funciones
 
-//Explicacion de lo que hace las funciones 
-
-// 1 odtiene o recive un Id y lo guarda en una variable 
-//2 crea una conexion a la base de datos 
+// 1 odtiene o recive un Id y lo guarda en una variable
+//2 crea una conexion a la base de datos
 // 3 pide a la base de daros encontrar el user Id
 //4 guarda el resultado de busqueda en la variable res
 
-//funcion que crea un contacto 
+//funcion que crea un contacto
 //1
- const createContact = async (req, res, next ) => {
+const createContact = async (req, res, next) => {
   const newcontact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
     favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    birthday: req.body.birthday,
   };
- //2 
- const response = await  mongodb
-   .getDb()
+  //2
+  const response = await mongodb
+    .getDb()
     .db()
-    .collection('contacts')
+    .collection("contacts")
     .insertOne(newcontact);
-    if (response.acknowledged) {
-      res.status(201).json(response);
-      console.log(' info saved');
-    } else {
-      res.status(500).json(response.error || 'Some error occurred while creating the contact.');
-      console.log('error');
-    }
-  };
-  
+  if (response.acknowledged) {
+    res.status(201).json(response);
+    console.log(" info saved");
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact."
+      );
+    console.log("error");
+  }
+};
 
-
-
-
-// --------------------------------------------------
-
-
-
-
-
-
-
+//*************Function to update a contact************************************************ */
 
 const updateContact = async (req, res) => {
-	//console.log('updateContact started');
-	const userId = new ObjectId(req.params.id);
-	const newInfo = {
-		firstName: req.body.firstName,
-		lastName: req.body.lastName,
-		email: req.body.email,
-		favoriteColor: req.body.favoriteColor,
-		birthday: req.body.birthday
-	};
-	const response = await mongodb    
-		.getDb()
-		.db()
-		.collection('contacts')
-		.replaceOne({ _id: userId }, newInfo);
-	if (response.acknowledged) {
-		res.status(204).json(response);
-		//console.log('Info saved to DB succesfully');
-	} else {
-		res.status(500).json(response.error || 'Some error occurred while creating the contact.');
-	//	console.log('Upload of info failed.');
-	}
+  const userId = new ObjectId(req.params.id);
+  const newInfo = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: userId }, newInfo);
+  if (response.acknowledged) {
+    res.status(204).json(response);
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact."
+      );
+  }
 };
 
 
-  
- 
-// module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
-module.exports = { getAll, getSingle, createContact, updateContact };
+/**************funcion para delete un contacto ************************************ */
+const deleteContact = async (req, res) => {
+  const userIdDelete = new ObjectID (req.params.id);
+  const response = await mongodb
+  .getDb()
+  .db()
+  .collection("contacts")
+  .deleteOne({ _id: userIdDelete}, true);
+  console.log('contactDelete');
+  if (response.acknowledged) {
+    res.status(204).json(response);
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while deleting the contact."
+      );
+  }
+};
+
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -11,6 +11,6 @@ router.post('/', contactsController.createContact);
 
 router.put('/:id', contactsController.updateContact);
 
-//router.delete('/:id', contactsController.deleteContact);
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
